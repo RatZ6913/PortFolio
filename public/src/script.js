5d8cc67a9109bb4973c09bda72dd0608bb854146
Pheak Rattana <rattana.pheak.rp@gmail.com>
2022-12-26 16:16:14 +0100

    Update Text Writer Multiple
    Update Text Writer Multiple

--- public/src/script.js
+++ public/src/script.js
@@ -1,7 +1,52 @@
 let nav = document.querySelector('nav');
 let menu = document.querySelector('#menu');
+let spanWords = document.querySelectorAll('.span-second-text');
 
 
+// Bouton : Menu Ouvrir/Fermer Navbar
 menu.addEventListener('click', () => {
   nav.classList.toggle('btn-menu');
-})
+});
+
+let arrTypeWriter = [
+  "Développement Web",
+  "FullStack",
+  "Compétences",
+  "Expérience"
+];
+
+// Text Writer : Pour les mots importants dans la partie Accueil
+spanWords.forEach((element, index) => {
+  let textWriter = arrTypeWriter[index];
+  let i = 0;
+
+  let interval = setInterval(() => {
+    element.textContent += textWriter[i];
+
+    if (i === textWriter.length) {
+      element.textContent = arrTypeWriter[index];
+      clearInterval(interval);
+
+      setTimeout(() => {
+        i = 0;
+        element.textContent = "";
+
+        setInterval(() => {
+          element.textContent += textWriter[i];
+          if (i === textWriter.length) {
+            element.textContent = textWriter;
+          } else {
+            i++;
+          }
+        }, 400)
+
+      }, 5000);
+
+    } else {
+      i++;
+    }
+  }, 200);
+});
+
+
+
